18d2f7f755ad52004f4b0f2c9b15e1fce15d248f
samugs <garcias110800@gmail.com>
2025-05-25 19:07:51 -0600

    Primer commit en nuevo repo

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<div class="container">
+<div class="">
     <!-- Card para el botón Agregar Categoria -->
     <div class="card cardModulo">
         <div class="encabezadoModulo">
@@ -18,41 +18,43 @@
     <!-- Card para la tabla de categorías -->
     <div class="card">
         <div class="card-body" style="overflow-y: auto;">
-            <table class="tablaPersonalizada">
-                <thead>
-                    <tr>                        
-                        <th>Nombre</th>
-                        <th>Estado</th>
-                        <th class="acciones">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach($categorias as $categoria)
-                    <tr>                        
-                        <td>{{ $categoria->nombre }}</td>
-                        <td>{{ $categoria->estado == '1' ? 'Activado' : 'Desactivado' }}</td>
-                        <td class="acciones">
-                            @can('Editar categorias')
-                            <a href="{{ route('categorias.edit', $categoria->id) }}" class="btn botonAcciones boton1">
-                                <i class="bi bi-pencil-square"></i> Actualizar
-                            </a>
-                            @endcan
+            <div class="table-responsive tablaResponsiveFix">
+                <table class="tablaPersonalizada">
+                    <thead>
+                        <tr>                        
+                            <th>Nombre</th>
+                            <th>Estado</th>
+                            <th class="acciones">Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($categorias as $categoria)
+                        <tr>                        
+                            <td>{{ $categoria->nombre }}</td>
+                            <td>{{ $categoria->estado == '1' ? 'Activado' : 'Desactivado' }}</td>
+                            <td class="acciones">
+                                @can('Editar categorias')
+                                <a href="{{ route('categorias.edit', $categoria->id) }}" class="btn botonAcciones boton1">
+                                    <i class="bi bi-pencil-square"></i> Actualizar
+                                </a>
+                                @endcan
 
-                            @can('Eliminar categorias')
-                            <form action="{{ route('categorias.desactivando', $categoria->id) }}" method="POST" style="display:inline;" id="form-estado-{{ $categoria->id }}">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" class="btn botonAcciones boton2 {{ $categoria->estado == 1 ? 'btn-danger' : 'btn-success' }}" id="btn-estado-{{ $categoria->id }}">
-                                    <i class="bi {{ $categoria->estado == 1 ? 'bi-x-circle' : 'bi-check-circle' }}"></i>
-                                    {{ $categoria->estado == 1 ? 'Desactivar' : 'Activar' }}
-                                </button>
-                            </form>
-                            @endcan
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                                @can('Eliminar categorias')
+                                <form action="{{ route('categorias.desactivando', $categoria->id) }}" method="POST" style="display:inline;" id="form-estado-{{ $categoria->id }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" class="btn botonAcciones boton2 {{ $categoria->estado == 1 ? 'btn-danger' : 'btn-success' }}" id="btn-estado-{{ $categoria->id }}">
+                                        <i class="bi {{ $categoria->estado == 1 ? 'bi-x-circle' : 'bi-check-circle' }}"></i>
+                                        {{ $categoria->estado == 1 ? 'Desactivar' : 'Activar' }}
+                                    </button>
+                                </form>
+                                @endcan
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
