6eebbed061f249dddc76abe7f1960a6df24e3171
samugs <garcias110800@gmail.com>
2025-06-05 01:13:24 -0600

    Comentario del commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,5 +23,22 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https'); //modifique esto para produccion en railway
         }
+
+        $dirs = [
+        'cortes',
+        'facturas',
+        'imagenes_productos',
+        'lotes',
+        'ventas',
+        ];
+
+        foreach ($dirs as $dir) {
+            Storage::disk('public')->makeDirectory($dir);
+        }
+
+        // Si también usás respaldos fuera de /public
+        if (!Storage::exists('respaldos_guardados')) {
+            Storage::makeDirectory('respaldos_guardados');
+        }
     }
 }
